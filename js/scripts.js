6db9464cc3d5da4e2606e1f98a65566e5da8eb9f
C3llarman <Jpdickman@gmail.com>
2023-04-02 14:59:46 -0700

    Changes will add a toggled menu to mobile screens
    addition of some js and css to the site gives it some more interactivity

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
 console.log("hHello world")
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle')
+    if (x.className === 'navtoggle') {
+       x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+}
